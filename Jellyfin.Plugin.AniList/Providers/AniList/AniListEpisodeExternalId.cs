6e7e837409da367460972a99a5322e30c46614fa
Dominik Procházka <dominikprochazka100@gmail.com>
2024-05-01 21:48:28 +0200

    seasons and episodes yay?

---
+++ Jellyfin.Plugin.AniList/Providers/AniList/AniListEpisodeExternalId.cs
@@ -0,0 +1,25 @@
+﻿using MediaBrowser.Controller.Entities.TV;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Entities;
+using MediaBrowser.Model.Providers;
+
+namespace Jellyfin.Plugin.AniList.Providers.AniList
+{
+    public class AniListEpisodeExternalId : IExternalId
+    {
+        public bool Supports(IHasProviderIds item)
+            => item is Episode;
+
+        public string ProviderName
+            => "AniList";
+
+        public string Key
+            => ProviderNames.AniList;
+
+        public ExternalIdMediaType? Type
+            => ExternalIdMediaType.Episode;
+
+        public string UrlFormatString
+            => "https://anilist.co/anime/{0}/";
+    }
+}
